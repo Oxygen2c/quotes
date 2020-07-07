19fd968e5c0e4608f51924064a11b007da16016c
Dmitriy <tdmitriy@rocketfirm.com>
2020-07-08 00:21:50 +0600

    deploy build

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
-var PrerenderSpaPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSpaPlugin.PuppeteerRenderer
-var path = require('path')
+// var PrerenderSpaPlugin = require('prerender-spa-plugin')
+// const Renderer = PrerenderSpaPlugin.PuppeteerRenderer
+// var path = require('path')
 
-module.exports = {
-  configureWebpack: config => {
-    if (process.env.NODE_ENV !== 'production') return
+// module.exports = {
+//   configureWebpack: config => {
+//     if (process.env.NODE_ENV !== 'production') return
 
-    return {
-      plugins: [
-        new PrerenderSpaPlugin({
-          staticDir: path.join(__dirname, 'dist'),
-          routes: ['/'],
-          renderer: new Renderer({
-            headless: true,
-            renderAfterTime: 5000
-          })
-        })
-      ]
-    }
-  }
-}
+//     return {
+//       plugins: [
+//         new PrerenderSpaPlugin({
+//           staticDir: path.join(__dirname, 'dist'),
+//           routes: ['/'],
+//           renderer: new Renderer({
+//             headless: true,
+//             renderAfterTime: 5000
+//           })
+//         })
+//       ]
+//     }
+//   }
+// }
